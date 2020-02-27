4af59fbee3da8cd2ff7589ba1e6b989f8869f66d
Hidenori Miyoshi <miyohide@gmail.com>
2020-02-27 21:27:07 +0900

    fix testMigrate task config.

--- buildSrc/src/main/groovy/io/pivotal/pal/tracker/gradlebuild/LocalMigrationPlugin.groovy
+++ buildSrc/src/main/groovy/io/pivotal/pal/tracker/gradlebuild/LocalMigrationPlugin.groovy
@@ -36,7 +36,7 @@ class LocalMigrationPlugin implements Plugin<Project> {
     private static FlywayExtension buildFlywayExtension(Project project, String dbName) {
         def ext = new FlywayExtension()
         ext.with {
-            url = "jdbc:mysql://localhost:3306/$dbName?useSSL=false&serverTimezone=UTC"
+            url = "jdbc:mysql://127.0.0.1:3306/$dbName?useSSL=false&serverTimezone=UTC"
             user = "tracker"
             outOfOrder = false
             locations = ["filesystem:${project.projectDir}"]
