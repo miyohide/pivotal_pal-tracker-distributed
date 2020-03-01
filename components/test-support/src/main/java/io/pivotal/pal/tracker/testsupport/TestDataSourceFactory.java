b3a409f630f3b2e4f5f033b30280b75fa3d3a265
Hidenori Miyoshi <miyohide@gmail.com>
2020-03-01 18:44:03 +0900

    change url and password

--- components/test-support/src/main/java/io/pivotal/pal/tracker/testsupport/TestDataSourceFactory.java
+++ components/test-support/src/main/java/io/pivotal/pal/tracker/testsupport/TestDataSourceFactory.java
@@ -10,8 +10,9 @@ public class TestDataSourceFactory {
     public static DataSource create(String name) {
         MysqlDataSource dataSource = new MysqlDataSource();
 
-        dataSource.setUrl("jdbc:mysql://localhost:3306/" + name + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false");
-        dataSource.setUser("tracker");
+        dataSource.setUrl("jdbc:mysql://127.0.0.1:3306/" + name + "?useSSL=false&useTimezone=true&serverTimezone=UTC&useLegacyDatetimeCode=false");
+        dataSource.setUser("root");
+        dataSource.setPassword("root");
 
         return dataSource;
     }
